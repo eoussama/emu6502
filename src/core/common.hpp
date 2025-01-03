d5c1ca10d757c24979dfa587abc38ca03fe17485
eoussama <oussama.essamadi@gmail.com>
2025-01-03 22:11:00 +0100

    feat: sandbox

---
+++ src/core/common.hpp
@@ -0,0 +1,13 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <cstdint>
+
+namespace Core
+{
+  using Byte = uint8_t;
+  using Word = uint16_t;
+  using DWord = uint32_t;
+}
+
+#endif
---
+++ src/sandbox/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "cpu.hpp"
+
+int main() {
+  using namespace Core;
+
+  Mem mem;
+  CPU cpu;
+
+  return 0;
+}
