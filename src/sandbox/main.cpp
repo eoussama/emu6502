0a8c6aaac2ff94bc9fba1761b5704a8c9f5f4791
eoussama <oussama.essamadi@gmail.com>
2025-01-04 11:53:11 +0100

    feat: program test

--- src/sandbox/main.cpp
+++ src/sandbox/main.cpp
@@ -6,10 +6,13 @@ int main()
 {
   using namespace Core;
 
-  Mem mem;
   CPU cpu;
+  Mem mem;
 
-  cpu.run(mem, 2);
+  Byte program[] = {0xA9, 0x03};
+
+  mem.load(program, sizeof(program), 0xFFFC);
+  cpu.run(mem, sizeof(program) / sizeof(Byte));
 
   return 0;
 }
