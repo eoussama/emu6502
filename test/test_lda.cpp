a538e029607e986c126800b087173a0dcbb4defb
eoussama <oussama.essamadi@gmail.com>
2025-01-04 18:39:22 +0100

    test: lda zero page x wrap test case

--- test/test_lda.cpp
+++ test/test_lda.cpp
@@ -22,15 +22,15 @@ namespace Core
     Mem mMem;
   };
 
-  TEST_F(LdaTest, LDA_IM)
+  TEST_F(LdaTest, LDAImmediateCanLoadAValueIntoTheARegister)
   {
     CPU cpuCopy = mCpu;
-    Byte program[] = {0xA9, 0x42};
+    Byte program[] = {0xA9, 0x84};
 
     mMem.load(program, sizeof(program), 0xFFFC);
     SDWord cycles = mCpu.run(mMem, 2);
 
-    EXPECT_EQ(mCpu.getA(), 0x42);
+    EXPECT_EQ(mCpu.getA(), 0x84);
     EXPECT_FALSE(mCpu.getFlags().Z);
     EXPECT_FALSE(mCpu.getFlags().N);
     EXPECT_EQ(mCpu.getFlags().C, cpuCopy.getFlags().C);
@@ -41,7 +41,7 @@ namespace Core
     EXPECT_EQ(cycles, 2);
   }
 
-  TEST_F(LdaTest, LDA_ZP)
+  TEST_F(LdaTest, LDAZeroPageCanLoadAValueIntoTheARegister)
   {
     CPU cpuCopy = mCpu;
     Byte program[1024 * 64] = {0};
@@ -64,7 +64,7 @@ namespace Core
     EXPECT_EQ(cycles, 3);
   }
 
-  TEST_F(LdaTest, LDA_ZPX)
+  TEST_F(LdaTest, LDAZeroPageXCanLoadAValueIntoTheARegister)
   {
     CPU cpuCopy = mCpu;
     Byte program[1024 * 64] = {0};
@@ -75,6 +75,30 @@ namespace Core
 
     mMem.load(program, sizeof(program));
     mCpu.setX(5);
+    SDWord cycles = mCpu.run(mMem, 4);
+
+    EXPECT_EQ(mCpu.getA(), 0x37);
+    EXPECT_FALSE(mCpu.getFlags().Z);
+    EXPECT_TRUE(mCpu.getFlags().N);
+    EXPECT_EQ(mCpu.getFlags().C, cpuCopy.getFlags().C);
+    EXPECT_EQ(mCpu.getFlags().V, cpuCopy.getFlags().V);
+    EXPECT_EQ(mCpu.getFlags().D, cpuCopy.getFlags().D);
+    EXPECT_EQ(mCpu.getFlags().I, cpuCopy.getFlags().I);
+    EXPECT_EQ(mCpu.getFlags().B, cpuCopy.getFlags().B);
+    EXPECT_EQ(cycles, 4);
+  }
+
+  TEST_F(LdaTest, LDAZeroPageXCanLoadValueIntoTheARegisterWhenItWraps)
+  {
+    CPU cpuCopy = mCpu;
+    Byte program[1024 * 64] = {0};
+
+    program[0xFFFC] = 0xB5;
+    program[0xFFFD] = 0x80;
+    program[0x007F] = 0x37;
+
+    mMem.load(program, sizeof(program));
+    mCpu.setX(0xFF);
     SDWord cycles = mCpu.run(mMem, 4);
 
     EXPECT_EQ(mCpu.getA(), 0x37);
