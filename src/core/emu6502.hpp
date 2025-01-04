8cc8328562a9a9850655e4d69bd2cd6fb7ae3e75
eoussama <oussama.essamadi@gmail.com>
2025-01-04 18:06:05 +0100

    refact: exported common and errors

--- src/core/emu6502.hpp
+++ src/core/emu6502.hpp
@@ -1,2 +1,9 @@
+#ifndef EMU6502_HPP
+#define EMU6502_HPP
+
 #include "cpu/cpu.hpp"
 #include "mem/mem.hpp"
+#include "common/common.hpp"
+#include "errors/errors.hpp"
+
+#endif
---
+++ src/core/errors/errors.hpp
@@ -0,0 +1,6 @@
+#ifndef ERRORS_HPP
+#define ERRORS_HPP
+
+#include "invalid_opcode.hpp"
+
+#endif
